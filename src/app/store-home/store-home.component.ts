b0fb7604e6e2929163523cd3ae41e34c9457a119
Lezcano Tech  <153436452+Oliver-Adamtey@users.noreply.github.com>
2024-01-10 15:56:45 +0000

    Update store-home.component.ts

--- src/app/store-home/store-home.component.ts
+++ src/app/store-home/store-home.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-store-home',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './store-home.component.html',
   styleUrl: './store-home.component.scss'
 })
 export class StoreHomeComponent {
 
+  name: string = '';
+  email: string = '';
+  message: string = '';
 }
