2995c0f901209d26cb3429bc15f3181a64d57968
Lezcano Tech  <153436452+Oliver-Adamtey@users.noreply.github.com>
2024-01-18 15:13:15 +0000

    new component created

---
+++ src/app/message-detail/message-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-message-detail',
+  standalone: true,
+  imports: [],
+  templateUrl: './message-detail.component.html',
+  styleUrl: './message-detail.component.scss'
+})
+export class MessageDetailComponent {
+
+}
