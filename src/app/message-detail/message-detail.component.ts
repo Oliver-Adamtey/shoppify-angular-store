8986afd1a97c7f52ee225eee82a8945ec68ced08
Lezcano Tech  <153436452+Oliver-Adamtey@users.noreply.github.com>
2024-01-22 16:31:41 +0000

    updated

--- src/app/message-detail/message-detail.component.ts
+++ src/app/message-detail/message-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-message-detail',
@@ -8,5 +8,6 @@ import { Component } from '@angular/core';
   styleUrl: './message-detail.component.scss'
 })
 export class MessageDetailComponent {
-
+  @Input()
+  message: any = {};
 }
